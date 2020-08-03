2289ed98a73f814cca4485e847b93e0125bbda13
watson <shihangbo@126.com>
2020-08-03 14:05:26 +0800

    回归；

--- source/vue/vdom/create-element.js
+++ source/vue/vdom/create-element.js
@@ -6,7 +6,8 @@ export function createElement(vm, tag,data={},...children) {
   }
   if(isReservedTag(tag)) { // 原始标签
     return vnode(tag,data,key,children,undefined)
-  } else { // 组件
+  } else { // 组件定义
+    console.log('组件的定义', vm, tag,data,children, vm.$options.components)
     let Ctor = vm.$options.components[tag] // 找到组件的定义 构造函数
     return createComponent(vm,tag,data,key,children,Ctor)
   }
@@ -14,6 +15,7 @@ export function createElement(vm, tag,data={},...children) {
 function createComponent(vm,tag,data,key,children,Ctor) {
   if (isObject(Ctor)) {
     Ctor = vm.$options._base.extend(Ctor)
+    console.log('组件的定义', Ctor)
   }
   data.hook = {
     init(vnode) {
